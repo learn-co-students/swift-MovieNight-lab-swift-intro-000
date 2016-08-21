5967a41bc6a12e444ef1259e693e370ca2dd9a8a
JimCampagno <jimcampagno@gmail.com>
2016-08-21 18:54:30 -0400

    Detail View pretty

---
+++ MovieSong/MovieDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MovieDetailViewController.swift
+//  MovieSong
+//
+//  Created by Jim Campagno on 8/21/16.
+//  Copyright © 2016 Gamesmith, LLC. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+
+    }
+    
+
+    
+    
+
+    
+
+}
