c4b036d205602735c9ca891a817bad236ad380f7
Mitchell Schuster <mgschuster11@gmail.com>
2017-07-24 18:43:13 +0300

    finished with what i could complete
    errors with new xcode/swift version

--- MovieSong/MovieDetailViewController.swift
+++ MovieSong/MovieDetailViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class MovieDetailViewController: UIViewController {
     
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var movieDirector: UILabel!
+    @IBOutlet weak var movieReleaseDate: UILabel!
+    @IBOutlet weak var movieRating: UILabel!
+    @IBOutlet weak var movieScores: UILabel!
+    @IBOutlet weak var moviePoster: UIImageView!
+    @IBOutlet weak var moviePlot: UITextView!
+    
+
     var movie: Movie!
     
     override func viewDidLoad() {
@@ -18,7 +27,7 @@ class MovieDetailViewController: UIViewController {
         handleLayoutOnLoad()
     }
     
-    override func viewWillAppear(animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
     
@@ -33,20 +42,42 @@ extension MovieDetailViewController {
         
         // TODO: Instruction #5, Set the .alpha property of all the views (outlets you created) here to 0.0
         
+        movieTitle.alpha = 0.0
+        movieDirector.alpha = 0.0
+        movieReleaseDate.alpha = 0.0
+        movieRating.alpha = 0.0
+        movieScores.alpha = 0.0
+        moviePoster.alpha = 0.0
+        moviePlot.alpha = 0.0
+        
     }
     
     func unHideAllViews() {
-        UIView.animateWithDuration(0.6, animations: {
+        UIView.animate(withDuration: 0.6, animations: {
             
             // TODO: Instruction #5, Set the .alpha property of all the views (outlets you created) here to 1.0
             
+            self.movieTitle.alpha = 1.0
+            self.movieDirector.alpha = 1.0
+            self.movieReleaseDate.alpha = 1.0
+            self.movieRating.alpha = 1.0
+            self.movieScores.alpha = 1.0
+            self.moviePoster.alpha = 1.0
+            self.moviePlot.alpha = 1.0
+    
             }, completion: nil)
     }
     
     func setupAllTheViews() {
         
         // TODO: Instruction #5, Implement the setupAllTheViews() function.
-       
+       movieTitle.text = movie.title
+        movieDirector.text = movie.director
+        movieReleaseDate.text = movie.released
+        movieRating.text = movie.rated
+        movieScores.text = "IMDB: \(movie.imdbRating) / rt: \(movie.tomatoMeter)%"
+        moviePoster.image = movie.image
+        moviePlot.text = movie.plot
         
         unHideAllViews()
     }
@@ -77,5 +108,8 @@ extension MovieDetailViewController {
 extension MovieDetailViewController {
     
     // TODO: Instruction #6, E.T. Go Home
+    
+    
+    
 
 }
